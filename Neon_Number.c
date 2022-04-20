5fdc1acd00c18f37cef8c181418af22e0a6cdc60
21A91A1255 <97424886+21A91A1255@users.noreply.github.com>
2022-04-20 19:55:17 +0530

    #include<stdio.h> int main() { 	int n,k,sum=0,d,i; 	scanf("%d",&n); 	k=n*n; 	for(i=0;i<n;i++) 	{ 		d=k%10; 		sum=sum+d; 		k=k/10; 	} 	if(n==sum) 	{ 		printf("Neon Number"); 	} 	else 	{ 		printf("Not Neon Number"); 	} }

--- Neon_Number.c
+++ Neon_Number.c
@@ -4,16 +4,12 @@ int main()
 	int n,k,sum=0,d,i;
 	scanf("%d",&n);
 	k=n*n;
-//	printf("%d
-",k);
-	while(k>0)
+	for(i=0;i<n;i++)
 	{
 		d=k%10;
 		sum=sum+d;
 		k=k/10;
 	}
-//	printf("%d
-",sum);
 	if(n==sum)
 	{
 		printf("Neon Number");
