c9016cb6ffb7061afa6cda60e5915b6e5ad2cf13
21A91A1255 <97424886+21A91A1255@users.noreply.github.com>
2022-07-02 18:30:01 +0530

    #include<stdio.h> int main() {     int i,n,arr[100],j,c=0,d=0,sum=0,k,p=0;     float avg;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {         c=0;         if(arr[i]==1)             continue;         for(j=2;j<arr[i];j++)         {             if(arr[i]%j==0)             {                 c++;             }         }         if(c==0)         {             if(arr[i]<=k)             {                 p++;             }         }
        }
            printf("%d",p);
    }

---
+++ primes_less_than_k.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,arr[100],j,c=0,d=0,sum=0,k,p=0;
+    float avg;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    { 
+        c=0;
+        if(arr[i]==1)
+            continue;
+        for(j=2;j<arr[i];j++)
+        {
+            if(arr[i]%j==0)
+            {
+                c++;
+            }
+        }
+        if(c==0)
+        {
+            if(arr[i]<=k)
+            {
+                p++;
+            }
+        }
+            
+    }
+        printf("%d",p);
+}
